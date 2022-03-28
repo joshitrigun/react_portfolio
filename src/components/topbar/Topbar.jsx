71341c4d2065d212daf66ddff09e7a533444980c
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-28 14:05:20 -0700

    top nav basic layout

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,9 +6,12 @@ const Topbar = () => {
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro_id">Intro</a>
+          <a href="#intro_id" className="logo">
+            Trigun.
+          </a>
         </div>
-        <div className="right"></div>
+
+        <div className="right">Right hand</div>
       </div>
     </div>
   );
