b6863d39ede7cd04348008923899a3effcb98cd8
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-28 11:54:04 -0700

    testimonials components added

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Testimonials.scss";
+
+const Testimonials = () => {
+  return <div className="testimonials">Testimonials</div>;
+};
+
+export default Testimonials;
