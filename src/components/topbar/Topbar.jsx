b024a6f76fc3b8c88900fbb29b9bd2177d0dd07d
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-29 17:01:38 -0700

    animation added to hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import "./Topbar.scss";
-
-const Topbar = () => {
+import PersonIcon from "@mui/icons-material/Person";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+const Topbar = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro_id" className="logo">
             Trigun.
           </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>+2368676365</span>
+          </div>
+          <div className="itemContainer">
+            <EmailOutlinedIcon />
+            <span>iamtrigun@gmail.com</span>
+          </div>
         </div>
 
-        <div className="right">Right hand</div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
