b7d0671d3f179cc9e646d29dce5a9a1635311c23
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-30 21:31:10 -0700

    side menu added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
+import Menu from "./components/menu/Menu";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
+
 import Contact from "./components/contact/Contact";
 import "./App.scss";
 
@@ -12,6 +14,7 @@ const App = () => {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
