5c4a967fb5d1541f8d32c630ac3f50674b871a1b
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-31 20:59:16 -0700

    portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,57 @@ import "./Portfolio.scss";
 const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
-      Portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web Application</li>
+        <li>E-commerce</li>
+        <li>Content</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://images.pexels.com/photos/9143162/pexels-photo-9143162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.pexels.com/photos/9143162/pexels-photo-9143162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.pexels.com/photos/9143162/pexels-photo-9143162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.pexels.com/photos/9143162/pexels-photo-9143162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.pexels.com/photos/9143162/pexels-photo-9143162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://images.pexels.com/photos/9143162/pexels-photo-9143162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 };
